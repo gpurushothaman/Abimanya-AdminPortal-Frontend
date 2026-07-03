c297ded12eb659ef451dcbc1d64127e53422aa1c
21ec043Thiyagu <146075318+21ec043Thiyagu@users.noreply.github.com>
2026-07-03 16:21:25 +0530

    updated doorthreshold

--- src/pages/doorthreshold/DoorThreshold.jsx
+++ src/pages/doorthreshold/DoorThreshold.jsx
@@ -1,28 +1,32 @@
 import React, { useEffect, useState } from 'react';
 import { getAllDoorThreshold, updateDoorThreshold } from '../../services/doorThresholdService';
 import { useToast } from '../../contexts/ToastContext';
-import Switch from '@mui/material/Switch';
-import { FormControl, InputLabel, MenuItem, Select, Stack, FormControlLabel } from '@mui/material';
+import { FormControl, InputLabel, MenuItem, Select, Stack } from '@mui/material';
 
-const JampLocation = () => {
+const DoorThreshold = () => {
   const { showToast } = useToast();
 
   const [designs, setDesigns] = useState([]);
+
   const [subDesigns, setSubDesigns] = useState([]);
+  const [frames, setFrames] = useState([]);
+  const [frameTypes, setFrameTypes] = useState([]);
+  const [frameTypeOptions, setFrameTypeOptions] = useState([]);
   const [threshold, setThreshold] = useState([]);
 
   const [selectedDesign, setSelectedDesign] = useState('');
   const [selectedSubDesign, setSelectedSubDesign] = useState('');
+  const [selectedFrame, setSelectedFrame] = useState('');
+  const [selectedFrameType, setSelectedFrameType] = useState('');
+  const [selectedFrameTypeOption, setSelectedFrameTypeOption] = useState('');
 
   useEffect(() => {
-    fetchDoorThreshold();
+    fetchDoorThresholds();
   }, []);
 
-  const fetchDoorThreshold = async () => {
+  const fetchDoorThresholds = async () => {
     try {
       const response = await getAllDoorThreshold();
-      console.log(response.data.data);
-      console.log('response data:', response);
       setDesigns(response.data.data);
     } catch (error) {
       console.error(error);
@@ -34,17 +38,16 @@ const JampLocation = () => {
       const response = await updateDoorThreshold(updateId, data);
 
       if (response?.data?.success) {
-        showToast('Threshold option updated successfully', 'success');
+        showToast('Door Threshold updated successfully', 'success');
       } else {
-        showToast('Threshold option not updated', 'error');
+        showToast('Door Threshold not updated', 'error');
       }
     } catch (error) {
       console.error(error);
+
       showToast('Something went wrong', 'error');
     }
   };
-
- 
 
   const handleEdit = (id, editing) => {
     setDesigns((prev) => {
@@ -52,23 +55,50 @@ const JampLocation = () => {
         ...design,
         subdesign: design.subdesign.map((sub) => ({
           ...sub,
-        threshold: sub.threshold.map((threshold) =>
-  threshold._id === id
-    ? { ...threshold, editing: !threshold.editing }
-    : threshold
-)
+          frame: sub.frame.map((frame) => ({
+            ...frame,
+            frameTypes: frame.frameTypes.map((type) => ({
+              ...type,
+              options: type.options.map((option) => ({
+                ...option,
+                thresholds: option.thresholds.map((threshold) =>
+                  threshold._id === id
+                    ? {
+                        ...threshold,
+                        editing: !threshold.editing
+                      }
+                    : threshold
+                )
+              }))
+            }))
+          }))
         }))
       }));
+
       const currentDesign = updated.find((d) => d._id === selectedDesign);
+
       const currentSub = currentDesign?.subdesign.find((s) => s._id === selectedSubDesign);
+
+      const currentFrame = currentSub?.frame.find((f) => f._id === selectedFrame);
+
+      const currentFrameType = currentFrame?.frameTypes.find((t) => t._id === selectedFrameType);
+
+      const currentOption = currentFrameType?.options.find((o) => o._id === selectedFrameTypeOption);
+
       setSubDesigns(currentDesign?.subdesign || []);
-      setThreshold(currentSub?.threshold || []);
+      setFrames(currentSub?.frame || []);
+      setFrameTypes(currentFrame?.frameTypes || []);
+      setFrameTypeOptions(currentFrameType?.options || []);
+      setThreshold(currentOption?.thresholds || []);
+
       if (editing) {
-        const result = currentSub?.threshold.find((f) => f._id === id);
+        const result = currentOption?.thresholds.find((o) => o._id === id);
+
         if (result) {
           saveThreshold(result, id);
         }
       }
+
       return updated;
     });
   };
@@ -79,49 +109,121 @@ const JampLocation = () => {
         ...design,
         subdesign: design.subdesign.map((sub) => ({
           ...sub,
-        threshold: sub.threshold.map((threshold) =>
-            threshold._id === id
-              ? {
-                  ...threshold,
-                  ...(category === 'data' ? { thresholdName: value } : { status: value })
-                }
-              : threshold
-          )
+          frame: sub.frame.map((frame) => ({
+            ...frame,
+            frameTypes: frame.frameTypes.map((type) => ({
+              ...type,
+              options: type.options.map((option) => ({
+                ...option,
+                thresholds: option.thresholds.map((threshold) =>
+                  threshold._id === id
+                    ? {
+                        ...threshold,
+                       
+                      }
+                    : threshold
+                )
+              }))
+            }))
+          }))
         }))
       }));
-      //Updated subdesign
+
       const currentDesign = updated.find((d) => d._id === selectedDesign);
-      setSubDesigns(currentDesign?.subdesign || []);
-      //Updated orientation
+
       const currentSub = currentDesign?.subdesign.find((s) => s._id === selectedSubDesign);
-      setThreshold(currentSub?.threshold || []);
+
+      const currentFrame = currentSub?.frame.find((f) => f._id === selectedFrame);
+
+      const currentFrameType = currentFrame?.frameTypes.find((t) => t._id === selectedFrameType);
+
+      const currentOption = currentFrameType?.options.find((o) => o._id === selectedFrameTypeOption);
+
+      setSubDesigns(currentDesign?.subdesign || []);
+      setFrames(currentSub?.frame || []);
+      setFrameTypes(currentFrame?.frameTypes || []);
+      setFrameTypeOptions(currentFrameType?.options || []);
+      setThreshold(currentOption?.thresholds || []);
+
       if (category === 'status') {
-        const result = currentSub?.threshold?.find((item) => item._id === id);
-        console.log('res:=', result);
-        saveThreshold(result, id);
+        const result = currentOption?.thresholds.find((threshold) => threshold._id === id);
+
+        if (result) {
+          saveThreshold(result, id);
+        }
       }
+
       return updated;
     });
   };
 
   const handleDesignChange = (e) => {
-    const designId = e.target.value;
-    setSelectedDesign(designId);
+    const id = e.target.value;
+
+    setSelectedDesign(id);
     setSelectedSubDesign('');
-    const design = designs.find((d) => d._id === designId);
-    setSubDesigns(design ? design.subdesign : []);
+    setSelectedFrame('');
+    setSelectedFrameType('');
+    setSelectedFrameTypeOption('');
+
+    const design = designs.find((d) => d._id === id);
+
+    setSubDesigns(design?.subdesign || []);
+    setFrames([]);
+    setFrameTypes([]);
+    setFrameTypeOptions([]);
     setThreshold([]);
   };
 
-
-
   const handleSubDesignChange = (e) => {
-    const subId = e.target.value;
-    setSelectedSubDesign(subId);
-    const sub = subDesigns.find((s) => s._id === subId);
-    setThreshold(sub ? sub.threshold : []);
+    const id = e.target.value;
+
+    setSelectedSubDesign(id);
+    setSelectedFrame('');
+    setSelectedFrameType('');
+    setSelectedFrameTypeOption('');
+
+    const sub = subDesigns.find((s) => s._id === id);
+
+    setFrames(sub?.frame || []);
+    setFrameTypes([]);
+    setFrameTypeOptions([]);
+    setThreshold([]);
   };
 
+  const handleFrameChange = (e) => {
+    const id = e.target.value;
+
+    setSelectedFrame(id);
+    setSelectedFrameType('');
+    setSelectedFrameTypeOption('');
+
+    const frame = frames.find((f) => f._id === id);
+
+    setFrameTypes(frame?.frameTypes || []);
+    setFrameTypeOptions([]);
+    setThreshold([]);
+  };
+
+  const handleFrameTypeChange = (e) => {
+    const id = e.target.value;
+    setSelectedFrameType(id);
+    setSelectedFrameTypeOption('');
+
+    const frameType = frameTypes.find((f) => f._id === id);
+    setFrameTypeOptions(frameType?.options || []);
+    setThreshold([]);
+  };
+
+  const handleFrameTypeOptionChange = (e) => {
+    const id = e.target.value;
+
+    setSelectedFrameTypeOption(id);
+
+    const option = frameTypeOptions.find((o) => o._id === id);
+
+    setThreshold(option?.thresholds || []);
+  };
   return (
     <div style={{ padding: '25px' }}>
       <h2 style={{ marginBottom: '20px', color: '#333', fontWeight: '600' }}>Door Threshold</h2>
@@ -149,8 +251,9 @@ const JampLocation = () => {
       color: "#66BB6A", // Dropdown arrow
     },
   }}  >
-          <InputLabel id="design-label">Design</InputLabel>
-          <Select labelId="design-label" value={selectedDesign} label="Design" onChange={handleDesignChange}>
+          <InputLabel>Design</InputLabel>
+
+          <Select value={selectedDesign} label="Design" onChange={handleDesignChange}>
             <MenuItem value="">
               <em>Select Design</em>
             </MenuItem>
@@ -184,9 +287,10 @@ const JampLocation = () => {
     "& .MuiSvgIcon-root": {
       color: "#66BB6A", // Dropdown arrow
     },
-  }}  >
-          <InputLabel id="subdesign-label">Sub Design</InputLabel>
-          <Select labelId="subdesign-label" value={selectedSubDesign} label="Sub Design" onChange={handleSubDesignChange}>
+  }} >
+          <InputLabel>Sub Design</InputLabel>
+
+          <Select value={selectedSubDesign} label="Sub Design" onChange={handleSubDesignChange}>
             <MenuItem value="">
               <em>Select Sub Design</em>
             </MenuItem>
@@ -194,6 +298,117 @@ const JampLocation = () => {
             {subDesigns.map((item) => (
               <MenuItem key={item._id} value={item._id}>
                 {item.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+
+        <FormControl fullWidth size="small" disabled={!selectedSubDesign}     sx={{
+    "& .MuiInputLabel-root": {
+      color: "#66BB6A", // Normal label color
+    },
+    "& .MuiInputLabel-root.Mui-focused": {
+      color: "#66BB6A", // Focus label color
+    },
+    "& .MuiOutlinedInput-root": {
+      "& fieldset": {
+        borderColor: "#A5D6A7", // Normal border
+      },
+      "&:hover fieldset": {
+        borderColor: "#66BB6A", // Hover border
+      },
+      "&.Mui-focused fieldset": {
+        borderColor: "#66BB6A", // Focus border
+      },
+    },
+    "& .MuiSvgIcon-root": {
+      color: "#66BB6A", // Dropdown arrow
+    },
+  }}  >
+          <InputLabel>Frame</InputLabel>
+
+          <Select value={selectedFrame} label="Frame" onChange={handleFrameChange}>
+            <MenuItem value="">
+              <em>Select Frame</em>
+            </MenuItem>
+
+            {frames.map((item) => (
+              <MenuItem key={item._id} value={item._id}>
+                {item.frameName}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+
+        <FormControl fullWidth size="small" disabled={!selectedFrame}       sx={{
+    "& .MuiInputLabel-root": {
+      color: "#66BB6A", // Normal label color
+    },
+    "& .MuiInputLabel-root.Mui-focused": {
+      color: "#66BB6A", // Focus label color
+    },
+    "& .MuiOutlinedInput-root": {
+      "& fieldset": {
+        borderColor: "#A5D6A7", // Normal border
+      },
+      "&:hover fieldset": {
+        borderColor: "#66BB6A", // Hover border
+      },
+      "&.Mui-focused fieldset": {
+        borderColor: "#66BB6A", // Focus border
+      },
+    },
+    "& .MuiSvgIcon-root": {
+      color: "#66BB6A", // Dropdown arrow
+    },
+  }} >
+          <InputLabel>Frame Type</InputLabel>
+
+          <Select value={selectedFrameType} label="Frame Type" onChange={handleFrameTypeChange}>
+            <MenuItem value="">
+              <em>Select Frame Type</em>
+            </MenuItem>
+
+            {frameTypes.map((item) => (
+              <MenuItem key={item._id} value={item._id}>
+                {item.frameTypeName}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+
+        <FormControl fullWidth size="small" disabled={!selectedFrameType}      sx={{
+    "& .MuiInputLabel-root": {
+      color: "#66BB6A", // Normal label color
+    },
+    "& .MuiInputLabel-root.Mui-focused": {
+      color: "#66BB6A", // Focus label color
+    },
+    "& .MuiOutlinedInput-root": {
+      "& fieldset": {
+        borderColor: "#A5D6A7", // Normal border
+      },
+      "&:hover fieldset": {
+        borderColor: "#66BB6A", // Hover border
+      },
+      "&.Mui-focused fieldset": {
+        borderColor: "#66BB6A", // Focus border
+      },
+    },
+    "& .MuiSvgIcon-root": {
+      color: "#66BB6A", // Dropdown arrow
+    },
+  }} >
+          <InputLabel>Frame Type Option</InputLabel>
+
+          <Select value={selectedFrameTypeOption} label="Frame Type Option" onChange={handleFrameTypeOptionChange}>
+            <MenuItem value="">
+              <em>Select Frame Type Option</em>
+            </MenuItem>
+
+            {frameTypeOptions.map((item) => (
+              <MenuItem key={item._id} value={item._id}>
+                {item.frameTypeOptionName}
               </MenuItem>
             ))}
           </Select>
@@ -211,10 +426,9 @@ const JampLocation = () => {
         }}
       >
         <div style={{ padding: '14px 18px', background: '#ccfb96', borderBottom: '1px solid #ddd', fontWeight: '600', fontSize: '16px' }}>
-          Threshold Options
+          Threshold Settings
         </div>
 
-       
         {threshold.map((item) => (
           <div
             key={item._id}
@@ -230,12 +444,12 @@ const JampLocation = () => {
               {item.editing ? (
                 <input
                   type="text"
-                value={item.thresholdName}
+                  value={item.thresholdName}
                   onChange={(e) => handleChange(item._id, e.target.value, 'data')}
                   style={{ padding: '6px 10px', width: '200px' }}
                 />
               ) : (
-                <span style={{ fontSize: '15px' }}>    {item.thresholdName}  </span>
+                <span style={{ fontSize: '15px' }}>{item.thresholdName}</span>
               )}
             </div>
 
@@ -253,16 +467,7 @@ const JampLocation = () => {
                 {item.editing ? '✔️' : '✏️'}
               </button>
 
-              <FormControlLabel
-                control={
-                  <Switch
-                    checked={item?.status || false}
-                    onChange={(e) => handleChange(item._id, e.target.checked, 'status')}
-                    color="success"
-                  />
-                }
-                label={item?.status ? 'Active' : 'Inactive'}
-              />
+            
             </div>
           </div>
         ))}
@@ -271,4 +476,4 @@ const JampLocation = () => {
   );
 };
 
-export default JampLocation;
+export default DoorThreshold;
